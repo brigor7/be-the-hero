5c313197e99e4bcf6775185214323718217b91f2
=Bruno Igor <=brigor7@gmail.com>
2021-01-11 16:31:59 -0300

    Implementação de Incident com dados do back/bd.

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
+import { useHistory } from 'react-router-dom';
 import './style.css';
 
+import api from '../../service/api';
 import logoImg from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 
 export default function NewIncident() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const history = useHistory();
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+    const data = { title, description, value };
+
+    try {
+      const response = await api.post('incidents', data);
+      alert(`Caso ${title} cadastrado.`);
+      history.push('/profile');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -25,12 +46,24 @@ export default function NewIncident() {
             Voltar
           </Link>
         </section>
-        <form>
-          <input placeholder="Título do caso"></input>
-          <textarea placeholder="Descreva o caso"></textarea>
-          <input placeholder="Valor do caso"></input>
+        <form onSubmit={handleNewIncident}>
+          <input
+            placeholder="Título do caso"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          ></input>
+          <textarea
+            placeholder="Descreva o caso"
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+          ></textarea>
+          <input
+            placeholder="Valor do caso"
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+          ></input>
 
-          <button class="button" onSubmit="/incident/new">
+          <button class="button" type="submit">
             Cadastrar
           </button>
         </form>
