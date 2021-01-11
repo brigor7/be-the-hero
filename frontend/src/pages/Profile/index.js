fbc787e97ec58021e4734b8c084170d392768684
=Bruno Igor <=brigor7@gmail.com>
2021-01-11 17:45:18 -0300

    Implementar consulta de dados de profile

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import './style.css';
+import api from '../../service/api';
 
 export default function Profile() {
+  const [incidents, setIncidents] = useState([]);
+  const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
+
+  useEffect(() => {
+    api.get('profile').then((response) => {
+      setIncidents(response.data);
+    });
+  }, [ongId]);
   return (
     <div className="profile-container">
       <header>
@@ -24,70 +33,23 @@ export default function Profile() {
       </header>
       <h1>Casos Cadastrados</h1>
       <div className="casos-container">
-        <ul>
-          <li>
-            <strong>Caso:</strong>
-            <p>Cadela atropelada</p>
-            <strong>Descrição</strong>
-            <p>
-              A cadelinha mai foi atropelada no bairro Parque 10 e teve que
-              passar por uma cirurgia
-            </p>
-            <strong>Valor</strong>
-            <p>R$ 1200.00</p>
-            <button>
-              <FiTrash2 size="18" color="#a8a8b3"></FiTrash2>
-            </button>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <strong>Caso</strong>
-            <p>Cadela atropelada</p>
-            <strong>Descrição</strong>
-            <p>
-              A cadelinha mai foi atropelada no bairro Parque 10 e teve que
-              passar por uma cirurgia
-            </p>
-            <strong>Valor</strong>
-            <p>R$ 1200.00</p>
-            <button>
-              <FiTrash2 size="18" color="#a8a8b3"></FiTrash2>
-            </button>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <strong>Caso</strong>
-            <p>Cadela atropelada</p>
-            <strong>Descrição</strong>
-            <p>
-              A cadelinha mai foi atropelada no bairro Parque 10 e teve que
-              passar por uma cirurgia
-            </p>
-            <strong>Valor</strong>
-            <p>R$ 1200.00</p>
-            <button>
-              <FiTrash2 size="18" color="#a8a8b3"></FiTrash2>
-            </button>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <strong>Caso</strong>
-            <p>Cadela atropelada</p>
-            <strong>Descrição</strong>
-            <p>
-              A cadelinha mai foi atropelada no bairro Parque 10 e teve que
-              passar por uma cirurgia
-            </p>
-            <strong>Valor</strong>
-            <p>R$ 1200.00</p>
-            <button>
-              <FiTrash2 size="18" color="#a8a8b3"></FiTrash2>
-            </button>
-          </li>
-        </ul>
+        {incidents.map((incident) => (
+          <ul key={incident.id}>
+            <li>
+              <strong>Caso:</strong>
+              <p>{incident.title}</p>
+
+              <strong>Descrição</strong>
+              <p>{incident.description}</p>
+
+              <strong>Valor</strong>
+              <p>{incident.value}</p>
+              <button>
+                <FiTrash2 size="18" color="#a8a8b3"></FiTrash2>
+              </button>
+            </li>
+          </ul>
+        ))}
       </div>
     </div>
   );
